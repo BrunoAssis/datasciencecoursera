ed913dd8a6c4f3ca7ca1044550240fb5d9d0d205
Bruno Assis <brunoassis@gmail.com>
2016-07-29 00:42:03 -0300

    Added Course3-Week2

---
+++ course3/week2/oauth.R
@@ -0,0 +1,26 @@
+library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. To make your own application, register at at
+#    https://github.com/settings/applications. Use any URL for the homepage URL
+#    (http://github.com is fine) and  http://localhost:1410 as the callback url
+#
+#    Replace your key and secret below.
+myapp <- oauth_app("github",
+                   key = "44767fab752515b24f3d",
+                   secret = "6097f4bbd828ea0f1fb502b8a1a3f4aaeb39ff7f")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+
+output <- content(req)
+repo <- sapply(output, function(repo) repo$name == "datasharing")[[1]]
+repo$created_at
---
+++ course3/week2/read_fwf.R
@@ -0,0 +1,7 @@
+file <- "data.for"
+lines <- readLines(file, n=10)
+w <- c(1, 9, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3)
+colNames <- c("filler", "week", "filler", "sstNino12", "filler", "sstaNino12", "filler", "sstNino3", "filler", "sstaNino3", "filler", "sstNino34", "filler", "sstaNino34", "filler", "sstNino4", "filler", "sstaNino4")
+d <- read.fwf(file, w, header=FALSE, skip=4, col.names=colNames)
+d <- d[, grep("^[^filler]", names(d))]
+sum(d[, 4])
